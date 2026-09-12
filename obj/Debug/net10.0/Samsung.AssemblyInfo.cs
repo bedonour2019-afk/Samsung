40b780940cc823aa3951a461109f6c812168f3b5
bedonour2019-afk <bedonour2019@gmail.com>
2026-09-12 22:08:27 +0300

    Move static files to root for vercel

--- obj/Debug/net10.0/Samsung.AssemblyInfo.cs
+++ obj/Debug/net10.0/Samsung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Samsung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ab41232c3eded7ea2e4ec8ea2aa80c30171e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Samsung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Samsung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
